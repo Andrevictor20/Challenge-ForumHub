a977d9b4e2a5062058687ed6d314a8870587139c
André Victor Macedo Pereira <victorandre280@gmail.com>
2025-08-24 01:31:02 -0300

    Configuração de timezone

--- src/main/java/com/br/alura/Challenge_ForumHub/ChallengeForumHubApplication.java
+++ src/main/java/com/br/alura/Challenge_ForumHub/ChallengeForumHubApplication.java
@@ -1,9 +1,12 @@
 package com.br.alura.Challenge_ForumHub;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableCaching
@@ -17,5 +20,9 @@ public class ChallengeForumHubApplication {
 
         SpringApplication.run(ChallengeForumHubApplication.class, args);
 	}
+    @PostConstruct
+    public void init(){
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Fortaleza"));
+    }
 
 }
