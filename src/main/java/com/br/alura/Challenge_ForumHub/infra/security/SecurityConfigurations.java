9a762b5abc7924a1e561d9a45682bd75c38904a8
André Victor Macedo Pereira <victorandre280@gmail.com>
2026-02-27 10:52:02 -0300

    Fix formatting in SecurityConfigurations.java

--- src/main/java/com/br/alura/Challenge_ForumHub/infra/security/SecurityConfigurations.java
+++ src/main/java/com/br/alura/Challenge_ForumHub/infra/security/SecurityConfigurations.java
@@ -29,7 +29,7 @@ public class SecurityConfigurations {
                 .authorizeHttpRequests(req -> {
                     req.requestMatchers(HttpMethod.POST, "/login").permitAll();
                     req.requestMatchers(HttpMethod.POST, "/usuarios").permitAll();
-                    req.requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**", /error).permitAll();
+                    req.requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**", "/error").permitAll();
                     req.anyRequest().authenticated();
                 })
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
